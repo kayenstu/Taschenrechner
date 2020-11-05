ec93cebc555d6abd176e85965f6f8e954da4a891
Xuan Khanh Nguyen Ha <wi19b019@technikum-wien.at>
2020-11-05 10:08:15 +0100

    Add project files.

---
+++ Taschenrechner/TrigonometricFunctionsSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace TrigonometrischeFunktionen
+{
+    [Binding]
+    public class TrigonometricFunctionsSteps
+    {
+        double result;
+
+        double angle;
+
+        [Given(@"the angle is (.*)")]
+        public void GivenTheAngleIs(double p0)
+        {
+            angle = p0;
+        }
+
+        [When(@"the sine function is chosen")]
+        public void WhenTheSineFunctionIsChosen()
+        {
+            result = Math.Sin(angle);
+        }
+
+        [When(@"the cosine function is chosen")]
+        public void WhenTheCosineFunctionIsChosen()
+        {
+            result = Math.Cos(angle);
+        }
+
+        [When(@"the tangen function is chosen")]
+        public void WhenTheTangenFunctionIsChosen()
+        {
+            result = Math.Tan(angle);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            double expected = p0;
+            double actual = result;
+            Assert.Equal(expected, result, 5);
+
+        }
+    }
+}
---
+++ Taschenrechner/trigonometrischeFunktionen.feature
@@ -0,0 +1,33 @@
+﻿Feature: trigonometric functions
+	In order to calculate the trigonometric functions
+	As a math idiot
+	I want to be told the sine, cosine and tangen of an angle in radius
+
+@mytag
+Scenario Outline: Calculate sine
+	Given the angle is <number>
+	When the sine function is chosen
+	Then the result should be <result>
+
+Examples: 
+	| number | result  |
+	| 0.5    | 0.47943 |
+	| 3.14159    | 0	   |
+
+Scenario Outline: Calculate cosine
+	Given the angle is <number>
+	When the cosine function is chosen
+	Then the result should be <result>
+
+Examples: 
+	| number | result  |
+	| 3.14159    | -1	   |
+
+Scenario Outline: Calculate tangen
+	Given the angle is <number>
+	When the tangen function is chosen
+	Then the result should be <result>
+
+Examples: 
+	| number| result  |
+	| 3.14159       | 0	   |
